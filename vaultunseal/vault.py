c63c33c4b2b096bc54137bda76def2e1aa89bb00
Donovan Harding <djcharding@outlook.com>
2021-01-22 03:07:24 -0500

    Fixing bugs

--- vaultunseal/vault.py
+++ vaultunseal/vault.py
@@ -11,23 +11,26 @@ class Vault:
     def __init__client(self):
         self._client = hvac.Client(url=self.url)
 
+    @property
+    def client(self):
+        return self._client
+
     def unseal(self, unseal_keys: list):
 
         CHECK_ATTEMPT_LIMIT = 3
         check_attempts = 0
 
-        if not self._client.sys.is_sealed():
+        if not self.client.sys.is_sealed():
             print('Vault is unsealed.')
         else:
             print('Unsealing vault...')
             try:
-                client.sys.submit_unseal_keys(keys=unseal_keys)
+                self.client.sys.submit_unseal_keys(keys=unseal_keys)
             except:
                 pass
-            
             while check_attempts < CHECK_ATTEMPT_LIMIT:
                 time.sleep(5)
-                if not client.sys.is_sealed():
+                if not self.client.sys.is_sealed():
                     check_attempts = CHECK_ATTEMPT_LIMIT
                     print('Vault successfully unsealed.')
                 else:
